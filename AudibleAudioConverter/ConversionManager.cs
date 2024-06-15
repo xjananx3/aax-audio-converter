706fae12b88ce2ae35a076b74c14b01e085c3b1d
Jan <janan9980@gmail.com>
2024-06-15 15:04:57 +0200

    (MainPage)ref: Added all conversion logic ConversionManager.cs and reformat

---
+++ AudibleAudioConverter/ConversionManager.cs
@@ -0,0 +1,134 @@
+using System.Diagnostics;
+using Aax.Activation.ApiClient;
+
+namespace AudibleAudioConverter;
+
+public class ConversionManager
+{
+    private Process _conversionProcess;
+    
+    public async Task StartConversionAsync(
+        string aaxFilePath,
+        string extractFolderPath,
+        string format,
+        string quality,
+        Action<double> updateProgress,
+        Action<string> updateStatus)
+    {
+        updateStatus("Getting File Hash");
+        var checksum = HashExtractor.GetActivationChecksum(aaxFilePath);
+        updateStatus("Getting Activation bytes");
+        var activationBytes = await AaxActivationClient.Instance.ResolveActivationBytes(checksum);
+        
+        var arguments = $"-y -activation_bytes {activationBytes} -i {aaxFilePath} -ab {quality} -map_metadata 0 -id3v2_version 3 -vn {Path.Combine(extractFolderPath, Path.GetFileNameWithoutExtension(aaxFilePath) + format)}";
+        var totalDuration = await GetFileDurationAsync(aaxFilePath);
+        
+        updateStatus("Converting File...");
+        await StartProcessAsync(arguments, totalDuration, updateProgress, updateStatus);
+    }
+    
+    private async Task StartProcessAsync(
+        string arguments, 
+        double totalDuration, 
+        Action<double> updateProgress, 
+        Action<string> updateStatus)
+    {
+        ProcessStartInfo startInfo = new ProcessStartInfo
+        {
+            FileName = "ffmpeg",
+            Arguments = arguments,
+            CreateNoWindow = true,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+        };
+        
+        _conversionProcess = new Process()
+        {
+            StartInfo = startInfo,
+            EnableRaisingEvents = true
+        };
+        
+        _conversionProcess.ErrorDataReceived += (sender, e) =>
+        {
+            if (!string.IsNullOrEmpty(e.Data) && e.Data.Contains("time="))
+            {
+                var percentage = ParseProgress(e.Data, totalDuration);
+                updateProgress(percentage);
+            }
+        };
+        
+        _conversionProcess.Start();
+        _conversionProcess.BeginErrorReadLine();
+        await Task.Run(() => _conversionProcess.WaitForExitAsync());
+        
+        updateStatus("Conversion Completed");
+    }
+    
+    public void CancelConversion()
+    {
+        if (_conversionProcess != null && !_conversionProcess.HasExited)
+        {
+            _conversionProcess.Kill();
+        }
+    }
+    
+    private async Task<double> GetFileDurationAsync(string filePath)
+    {
+        var tcs = new TaskCompletionSource<double>();
+        
+        ProcessStartInfo startInfo = new ProcessStartInfo
+        {
+            FileName = "ffprobe",
+            Arguments = $"-v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 \"{filePath}\"",
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+        
+        using (var process = new Process())
+        {
+            process.StartInfo = startInfo;
+            process.OutputDataReceived += (sender, e) =>
+            {
+                if (double.TryParse(e.Data, out double duration))
+                {
+                    tcs.SetResult(duration);
+                }
+            };
+            process.Start();
+            process.BeginOutputReadLine();
+            
+            await process.WaitForExitAsync();
+        }
+        
+        return await tcs.Task;
+    }
+    
+    private double ParseProgress(string data, double totalDuration)
+    {
+        try
+        {
+            var timeIndex = data.IndexOf("time=", StringComparison.Ordinal);
+            if (timeIndex != -1)
+            {
+                var timeString = data.Substring(timeIndex + 5, 11);
+                var timeSpan = TimeSpan.ParseExact(timeString, @"hh\:mm\:ss\.ff", null);
+                
+                if (totalDuration > 0)
+                {
+                    return timeSpan.TotalSeconds / totalDuration * 100;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error parsing progress: {ex.Message}");
+            
+            return 0;
+        }
+        return 0;
+    }
+    
+    
+}
--- AudibleAudioConverter/MainPage.xaml.cs
+++ AudibleAudioConverter/MainPage.xaml.cs
@@ -8,6 +8,7 @@ namespace AudibleAudioConverter;
 public sealed partial class MainPage : Page
 {
     private readonly FileProvider _fileProvider = new();
+    private readonly ConversionManager _conversionManager = new();
     public MainPage()
     {
         InitializeComponent();
@@ -33,157 +34,28 @@ public sealed partial class MainPage : Page
     {
         DisableElements();
         StartBar();
-        StatusTextBlock.Visibility = Visibility.Visible;
         
+        await _conversionManager.StartConversionAsync(
+            AaxFileDisplay.Text,
+            ExtractFolderDisplay.Text,
+            GetSelectedFormat(),
+            GetSelectedQuality(),
+            UpdateProgressBar,
+            ShowStatus);
         
-        await Conversion();
-        
+        EnableElements();
+        StopBar();
+        OpenOutputButton.Visibility = Visibility.Visible;
     }
     
-    private async Task Conversion()
+    private void ShowStatus(string message)
     {
-        var checksum = GetHash();
-        var activationBytes = await GetBytes(checksum);
-        var arguments = GetArguments(activationBytes);
-        var totalDuration = await GetFileDurationAsync();
-        await StartConversion(arguments, totalDuration);
+        StatusTextBlock.Text = message;
     }
     
-    private async Task StartConversion(string arguments, double totalDuration)
+    private void UpdateProgressBar(double percentage)
     {
-        StatusTextBlock.Text = "Converting File...";
-        ProgressBar.Value = 0;
-        ProgressBar.Visibility = Visibility.Visible;
-        
-        ProcessStartInfo startInfo = new ProcessStartInfo
-        {
-            FileName = "ffmpeg",
-            Arguments = arguments,
-            CreateNoWindow = true,
-            RedirectStandardOutput = true,
-            RedirectStandardError = true,
-            UseShellExecute = false,
-        };
-        
-        Process process = new Process()
-        {
-            StartInfo = startInfo,
-            EnableRaisingEvents = true
-        };
-        
-        process.OutputDataReceived += (sender, e) => WriteToLog(e.Data);
-        process.ErrorDataReceived += (sender, e) => ProcessErrorData(e.Data, totalDuration);
-        process.Start();
-        process.BeginOutputReadLine();
-        process.BeginErrorReadLine();
-        
-        await Task.Run(() => process.WaitForExitAsync());
-        
-        Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-        {
-            StopBar();
-            EnableElements();
-            StatusTextBlock.Text = "Conversion Completed";
-            OpenOutputButton.Visibility = Visibility.Visible;
-        });
-    }
-    
-    private async void ProcessErrorData(string data, double totalDuration)
-    {
-        if(string.IsNullOrEmpty(data)) return;
-        
-        if (data.Contains("time="))
-        {
-            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-            {
-                double percentage = ParseProgress(data, totalDuration);
-                ProgressBar.Value = percentage;
-            });
-        }
-    }
-    
-    private double ParseProgress(string data, double totalDuration)
-    {
-        try
-        {
-            var timeIndex = data.IndexOf("time=", StringComparison.Ordinal);
-            if (timeIndex != -1)
-            {
-                var timeString = data.Substring(timeIndex + 5, 11);
-                var timeSpan = TimeSpan.ParseExact(timeString, @"hh\:mm\:ss\.ff", null);
-                
-                if (totalDuration > 0)
-                {
-                    return timeSpan.TotalSeconds / totalDuration * 100;
-                }
-            }
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine($"Error parsing progress: {ex.Message}");
-            
-            return 0;
-        }
-        
-        return 0;
-    }
-    
-    private async Task<double> GetFileDurationAsync()
-    {
-        var tcs = new TaskCompletionSource<double>();
-        
-        ProcessStartInfo startInfo = new ProcessStartInfo
-        {
-            FileName = "ffprobe",
-            Arguments = $"-v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 \"{AaxFileDisplay.Text}\"",
-            RedirectStandardOutput = true,
-            UseShellExecute = false,
-            CreateNoWindow = true
-        };
-        
-        using (var process = new Process())
-        {
-            process.StartInfo = startInfo;
-            process.OutputDataReceived += (sender, e) =>
-            {
-                if (double.TryParse(e.Data, out double duration))
-                {
-                    tcs.SetResult(duration);
-                }
-            };
-            process.Start();
-            process.BeginOutputReadLine();
-            
-            await process.WaitForExitAsync();
-        }
-        
-        return await tcs.Task;
-    }
-    
-    private void WriteToLog(string data)
-    {
-        if (!string.IsNullOrEmpty(data))
-        {
-            // Hier könntest du zusätzliche Ausgaben verarbeiten oder protokollieren
-            Debug.WriteLine($"ffmpeg Output: {data}");
-        }
-    }
-    
-    private string GetArguments(object activationBytes)
-    {
-        StatusTextBlock.Text = "Getting Arguments";
-        
-        string fileout = Path.Combine(ExtractFolderDisplay.Text, Path.GetFileNameWithoutExtension(AaxFileDisplay.Text) + GetOutExtension());
-        string quality = GetSelectedQuality();
-        
-        string arg = @"-y -activation_bytes ";
-        string arg1 = @" -i ";
-        string arg2 = @" -ab ";
-        string arg3 = @" -map_metadata 0 -id3v2_version 3 -vn ";
-        
-        string arguments = arg + activationBytes + arg1 + AaxFileDisplay.Text + arg2 + quality + arg3 + fileout;
-        
-        return arguments;
+        ProgressBar.Value = percentage;
     }
     
     
@@ -200,7 +72,7 @@ public sealed partial class MainPage : Page
         }
     }
     
-    private string GetOutExtension()
+    private string GetSelectedFormat()
     {
         switch (FormatComboBox.SelectedIndex)
         {
@@ -209,24 +81,6 @@ public sealed partial class MainPage : Page
             case 2: return ".flac";
             default: return ".mp3";
         }
-    }
-    
-    private async Task<string> GetBytes(string checksum)
-    {
-        StatusTextBlock.Text = "Getting Activation bytes";
-        
-        var activationBytes = await AaxActivationClient.Instance.ResolveActivationBytes(checksum);
-        
-        return activationBytes;
-    }
-    
-    private string GetHash()
-    {
-        StatusTextBlock.Text = "Getting File Hash";
-        
-        var checksum = HashExtractor.GetActivationChecksum(AaxFileDisplay.Text);
-        
-        return checksum;
     }
     
     private void DisableElements()
